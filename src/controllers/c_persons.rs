a47cec6c3f489e5588603b8524c19826fc97d52b
novabyte <evan.t.tellep@gmail.com>
2024-12-15 12:16:20 -0500

    adjusting cookie expiration time

--- src/controllers/c_persons.rs
+++ src/controllers/c_persons.rs
@@ -239,7 +239,9 @@ fn generate_refresh_cookie<'a>(refresh_token: Option<String>) -> Cookie<'a> {
 
     // DEBT: may want to make a specific path for logout and refresh to more granularly control the cookie
     let cookie_path = "/api/persons";
-    let valid_until = OffsetDateTime::now_utc() + Duration::days(refresh_duration);
+
+    // set cookie to expire an hour after the token is set to expire
+    let valid_until = OffsetDateTime::now_utc() + Duration::minutes(refresh_duration + 60);
 
     // TODO: make this env key a constant
     let is_secure = env::var("USE_TLS")
